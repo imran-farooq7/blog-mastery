f5693f9979fcefc41682d15eb0174713871d5c59
Imran Farooq <imran_farooq7@hotmail.com>
2023-12-22 16:06:33 +0500

    Navbar component build

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark py-3">
+			<div className="container">
+				<Link href="/" className="navbar-brand">
+					<Image
+						src={"/images/logo.png"}
+						alt="logo"
+						width={225}
+						height={25}
+						priority
+					/>
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavDropdown"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul className="navbar-nav ms-auto d-flex align-items-center">
+						<li className="nav-item">
+							<Link href={"/"} className="nav-link fw-medium">
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link href={"#details"} className="nav-link fw-medium">
+								Details
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link href={"/contact"} className="nav-link fw-medium">
+								<button
+									type="button"
+									className="btn btn-outline-light navbar-btn"
+								>
+									Contact
+								</button>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+export default Navbar;
